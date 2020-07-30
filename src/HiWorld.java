3a96a6ee7c9b58685a5977da7feb75d9eaa51537
Calculator <jsh_19869@163.com>
2020-07-30 18:03:16 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,5 +1,8 @@
 public class HiWorld {
     public static void main(String[] args) {
         System.out.println("Hi world!!!");
+        for(int i=0;i<3;i++){
+            System.out.println(i);
+        }
     }
 }
